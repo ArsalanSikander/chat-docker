bedac476bc27cf67342f1cca2b5dbcc37eec7703
ArsalanSikander <arsalansikander0216@gmail.com>
2024-12-26 14:46:23 +0500

    Test and Logout button

--- tests/one.js
+++ tests/one.js
@@ -1,32 +1,51 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+const driver = new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(new chrome.Options().addArguments(`--webdriver.chrome.driver=E:/newDownloads/chromedriver-win64/chromedriver-win64/chromedriver.exe`))
+    .build();
+
+
+async function getPage(link) {
+    await driver.get(link);
+    return true;
+}
+
+async function waitForTitle(result) {
+    if (result) {
+        await driver.wait(until.titleIs("React App"), 10000);
+        return true;
+    }
+}
+
+async function checkTitle(result) {
+    if (result) {
+        await driver.getTitle().then(title => {
+
+            if (title === 'React App') {
+                console.log("Test Passed!")
+            } else {
+                console.log("Test Failed! Title was <" + title + ">");
+            }
+        });
+        return true;
+    }
+}
 
 async function example() {
     // Set the path to the ChromeDriver executable
-    const driver = new Builder()
-        .forBrowser('chrome')
-        .setChromeOptions(new chrome.Options().addArguments(`--webdriver.chrome.driver=E:/newDownloads/chromedriver-win64/chromedriver-win64/chromedriver.exe`).addArguments('--headless'))
-        .build();
+
 
     try {
-        await driver.get('http:/www.pCloudy.com').then(async function () {
-            await driver.getTitle().then(title => {
 
-                if (title.localeCompare("Mobile App Testing, Continuous Testing Cloud, Mobile Testing Tools | pCloudy")) {
-                    console.log("Test Passed!")
-                } else {
-                    console.log("Test Failed!")
-                }
-            })
-        });
-    } finally {
+        const r1 = await getPage('http://localhost:3000/login');
+        const r2 = await waitForTitle(r1);
+        const r3 = await checkTitle(r2);
+
+    }
+    finally {
         await driver.quit();
     }
 }
 
 example();
-
-// test cases i can make
-// 1. page loads on app launch
-// 2. Login occurs on entering correct credentials 
-// 3. 
